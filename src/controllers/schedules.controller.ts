2b85619e1aa995da762b9f3cdd218129efad79f0
Cézar Fernando <cezarfbc@gmail.com>
2023-07-10 16:41:23 -0300

    Update schedules.controller.ts

--- src/controllers/schedules.controller.ts
+++ src/controllers/schedules.controller.ts
@@ -3,54 +3,59 @@ import { SchedulesService } from "../services/schedules.service";
 import { parseISO } from "date-fns";
 
 class SchedulesController {
-    private schedulesService: SchedulesService
-    constructor() {
-        this.schedulesService = new SchedulesService()
-    }
-    async store(request: Request , response: Response, next: NextFunction) {
-        const { name, phone, date } = request.body
-        const { user_id } = request 
-        try {
-            const result = await this.schedulesService.create({name, phone, date, user_id})
+  private schedulesService: SchedulesService;
+  constructor() {
+    this.schedulesService = new SchedulesService();
+  }
+  async store(request: Request, response: Response, next: NextFunction) {
+    const { name, phone, date } = request.body;
+    const { user_id } = request;
+    try {
+      const result = await this.schedulesService.create({
+        name,
+        phone,
+        date,
+        user_id,
+      });
 
-            return response.status(201).json(result)
-        } catch (error) {
-            next(error);
-        }
+      return response.status(201).json(result);
+    } catch (error) {
+      next(error);
     }
-    async index(request: Request, response: Response, next: NextFunction) {
-        const { date } = request.query
-        const { user_id } = request 
-        const parseDate = date ? parseISO(date.toString()) : new Date()
-        try {
-            const result = await this.schedulesService.index(parseDate, user_id)
-            
-            return response.json(result)
-        } catch (error) {
-            next(error);
-        }
-    }
-    async update(request: Request, response: Response, next: NextFunction) {
-        const { id } = request.params
-        const { date } = request.body
-        const { user_id } = request
-        try {
-            const result = await this.schedulesService.update(id, date, user_id)
+  }
+  async index(request: Request, response: Response, next: NextFunction) {
+    const { date } = request.query;
+    const { user_id } = request;
+    const parseDate = date ? parseISO(date.toString()) : new Date();
+    try {
+      const result = await this.schedulesService.index(parseDate, user_id);
 
-            return response.json(result)
-        } catch (error) {
-            next(error);
-        }
+      return response.json(result);
+    } catch (error) {
+      next(error);
     }
-    async delete(request: Request, response: Response, next: NextFunction) {
-        const {id} = request.params
-        try {
-            const result = await this.schedulesService.delete(id)
+  }
+  async update(request: Request, response: Response, next: NextFunction) {
+    const { id } = request.params;
+    const { date } = request.body;
+    const { user_id } = request;
+    try {
+      const result = await this.schedulesService.update(id, date, user_id);
 
-            return response.json(result)
-        } catch (error) {
-            next(error)
-        }
+      return response.json(result);
+    } catch (error) {
+      next(error);
     }
+  }
+  async delete(request: Request, response: Response, next: NextFunction) {
+    const { id } = request.params;
+    try {
+      const result = await this.schedulesService.delete(id);
+
+      return response.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
-export { SchedulesController }
+export { SchedulesController };
