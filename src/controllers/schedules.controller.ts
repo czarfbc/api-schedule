3859f448af473c5ba8da2b2a90e7784e9d563e4f
Cezar Fernando <czarfbc@gmail.com>
2023-10-24 15:59:04 -0300

    statuscode consertado

--- src/controllers/schedules.controller.ts
+++ src/controllers/schedules.controller.ts
@@ -73,7 +73,7 @@ class SchedulesController {
     try {
       const result = await this.schedulesService.delete(id);
 
-      return response.status(20).json(result);
+      return response.status(204).json(result);
     } catch (error) {
       next(error);
     }
