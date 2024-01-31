841377799223e0797f8fd6f6fed7b4c6362b5aaf
Cezar Fernando <czarfbc@gmail.com>
2024-01-31 14:42:45 -0300

    zoderrror

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -8,15 +8,15 @@ export const errorsMiddleware = (
   response: Response,
   next: NextFunction
 ) => {
-  const statusCode = error.statusCode ?? 500;
-  const message = error.statusCode ? error.message : 'Internal Server Error';
-
   if (error instanceof ZodError) {
     const validationErrors = error.errors.map((err) => err.message);
     return response
       .status(400)
       .json({ statusCode: 400, message: validationErrors });
   }
+
+  const statusCode = error.statusCode ?? 500;
+  const message = error.statusCode ? error.message : 'Internal Server Error';
 
   return response.status(statusCode).json({ statusCode, message });
 };
